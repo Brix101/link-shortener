eec1359d7adddf3755435117a265c5625dbc62a7
Brixter Porras <brixterporras@gmail.com>
2022-08-25 21:49:38 +0800

    Added Components

--- src/pages/api/get-url/[slug].ts
+++ src/pages/api/get-url/[slug].ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { prisma } from "../../db/client";
+import { prisma } from "../../../db/client";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const slug = req.query["slug"];
@@ -27,6 +27,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
     return;
   }
+
   res.setHeader("Content-Type", "application/json");
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Cache-Control", "s-maxage=1000000000, stale-while-revalidate");
